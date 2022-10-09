1c867e5b9dd3bfd8c14bbc3d284e0bcba7ecf9d3
matrix <113445886@qq.com>
2022-10-09 15:48:38 +0800

    feat: new stag & prod stack

--- index.ts
+++ index.ts
@@ -18,69 +18,75 @@ const tags = {
   Stack: `Pulumi-${stack}`,
 };
 
-const ecr = new aws.ecr.Repository(name, {
-  name,
-  tags,
-});
+let ecr;
 
-new aws.ecr.RepositoryPolicy(
-  tags.Name,
-  {
-    repository: ecr.id,
-    policy: JSON.stringify({
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Sid: "new policy",
-          Effect: "Allow",
-          Principal: "*",
-          Action: [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:DescribeRepositories",
-            "ecr:GetRepositoryPolicy",
-            "ecr:ListImages",
-            "ecr:DeleteRepository",
-            "ecr:BatchDeleteImage",
-            "ecr:SetRepositoryPolicy",
-            "ecr:DeleteRepositoryPolicy",
-          ],
-        },
-      ],
-    }),
-  },
-  { deleteBeforeReplace: true }
-);
+if (stack == "dev") {
+  ecr = new aws.ecr.Repository(name, {
+    name,
+    tags,
+  });
 
-new aws.ecr.LifecyclePolicy(
-  tags.Name,
-  {
-    repository: ecr.id,
-    policy: JSON.stringify({
-      rules: [
-        {
-          rulePriority: 1,
-          description: "Expire images older than 14 days",
-          selection: {
-            tagStatus: "untagged",
-            countType: "sinceImagePushed",
-            countUnit: "days",
-            countNumber: 14,
+  new aws.ecr.RepositoryPolicy(
+    tags.Name,
+    {
+      repository: ecr.id,
+      policy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Sid: "new policy",
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:PutImage",
+              "ecr:InitiateLayerUpload",
+              "ecr:UploadLayerPart",
+              "ecr:CompleteLayerUpload",
+              "ecr:DescribeRepositories",
+              "ecr:GetRepositoryPolicy",
+              "ecr:ListImages",
+              "ecr:DeleteRepository",
+              "ecr:BatchDeleteImage",
+              "ecr:SetRepositoryPolicy",
+              "ecr:DeleteRepositoryPolicy",
+            ],
           },
-          action: {
-            type: "expire",
+        ],
+      }),
+    },
+    { deleteBeforeReplace: true }
+  );
+
+  new aws.ecr.LifecyclePolicy(
+    tags.Name,
+    {
+      repository: ecr.id,
+      policy: JSON.stringify({
+        rules: [
+          {
+            rulePriority: 1,
+            description: "Expire images older than 14 days",
+            selection: {
+              tagStatus: "untagged",
+              countType: "sinceImagePushed",
+              countUnit: "days",
+              countNumber: 14,
+            },
+            action: {
+              type: "expire",
+            },
           },
-        },
-      ],
-    }),
-  },
-  { deleteBeforeReplace: true }
-);
+        ],
+      }),
+    },
+    { deleteBeforeReplace: true }
+  );
+} else {
+  ecr = aws.ecr.Repository.get(name, name);
+}
 
 export const ecrRepositoryName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
