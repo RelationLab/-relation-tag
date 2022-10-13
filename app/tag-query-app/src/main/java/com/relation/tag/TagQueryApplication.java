7b69be0b640173d18784c69297e20462b70ad61f
panzuchao <panzuchao@126.com>
2022-10-13 14:54:38 +0800

    add test

--- app/tag-query-app/src/main/java/com/relation/tag/TagQueryApplication.java
+++ app/tag-query-app/src/main/java/com/relation/tag/TagQueryApplication.java
@@ -1,31 +1,15 @@
 package com.relation.tag;
 
-import com.relation.tag.request.GetAddressLabelsRequest;
-import com.relation.tag.service.AddressLabelService;
-import org.assertj.core.util.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.relation.tag"}, exclude = {ElasticsearchDataAutoConfiguration.class})
 public class TagQueryApplication {
     org.springframework.core.io.support.SpringFactoriesLoader SpringFactoriesLoader;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(TagQueryApplication.class, args);
-        AddressLabelService addressLabelService = context.getBean(AddressLabelService.class);
-//        List<GetAddressLabelsResponse> list =  addressLabelService.findByAddress(GetAddressLabelRequest.builder()
-//                .input(GetAddressLabelRequest.AddressInput.builder().address("0x0308472078b66bcf7f00f2e3d5a12e44f7ffb829")
-//                        .build()).build());
-
-        GetAddressLabelsRequest.Input input = GetAddressLabelsRequest.Input.builder().baseId(100L)
-                .labels(Lists.newArrayList("ALL_ALL_ALL_ACTIVITY_L1")).limit(10).build();
-        GetAddressLabelsRequest request = GetAddressLabelsRequest.builder().input(input).build();
-        Long count = addressLabelService.getAddressCount(request);
-
-//        List<GetAddressLabelsResponse> list =  addressLabelService.getAddressLabels(request);
-        System.out.println();
+        SpringApplication.run(TagQueryApplication.class, args);
     }
 
 }
--- app/tag-query-app/src/test/java/com/relation/tag/TagQueryAppApplicationTests.java
+++ app/tag-query-app/src/test/java/com/relation/tag/TagQueryAppApplicationTests.java
@@ -1,13 +1,51 @@
 package com.relation.tag;
 
+import com.relation.tag.request.GetAddressLabelRequest;
+import com.relation.tag.request.GetAddressLabelsRequest;
+import com.relation.tag.response.GetAddressLabelsResponse;
+import com.relation.tag.service.AddressLabelService;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TagQueryAppApplicationTests {
+
+    @Autowired
+    private AddressLabelService addressLabelService;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void getAddressLabelsTest() {
+        GetAddressLabelsRequest.Input input = GetAddressLabelsRequest.Input.builder().baseId(100L)
+                .labels(Lists.newArrayList("ALL_ALL_ALL_ACTIVITY_L1")).limit(10).build();
+        GetAddressLabelsRequest request = GetAddressLabelsRequest.builder().input(input).build();
+        List<GetAddressLabelsResponse> getAddressLabels = addressLabelService.getAddressLabels(request);
+        System.out.println();
+    }
+
+    @Test
+    void getAddressCountTest() {
+        GetAddressLabelsRequest.Input input = GetAddressLabelsRequest.Input.builder().baseId(100L)
+                .labels(Lists.newArrayList("ALL_ALL_ALL_ACTIVITY_L1")).limit(10).build();
+        GetAddressLabelsRequest request = GetAddressLabelsRequest.builder().input(input).build();
+        Long getAddressCount = addressLabelService.getAddressCount(request);
+        System.out.println();
+    }
+
+    @Test
+    void findByAddressTest() {
+        List<GetAddressLabelsResponse> findByAddress = addressLabelService.findByAddress(
+                GetAddressLabelRequest.builder()
+                .input(GetAddressLabelRequest.AddressInput.builder()
+                        .address("0x0308472078b66bcf7f00f2e3d5a12e44f7ffb829")
+                        .build()).build());
+        System.out.println();
+    }
 }
